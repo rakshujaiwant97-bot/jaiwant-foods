ada9b90d5d99eb1254ff46c8073e5747eefe8cba
Rakshita Jaiwant <rakshujaiwant97@gmail.com>
2026-08-17 10:37:23 +0530

    Connect app to Aiven MySQL

--- database.py
+++ database.py
@@ -1,12 +1,15 @@
 import mysql.connector
+import os
 
 
 def get_db_connection():
     connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Jaiwant@123",
-        database="jaiwantfoods"
+        host=os.environ.get("DB_HOST"),
+        port=int(os.environ.get("DB_PORT", 3306)),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME"),
+        ssl_disabled=False
     )
 
     return connection
